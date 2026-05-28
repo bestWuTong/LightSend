f546cd65c425aafeb3a8a28c6ae4efe8d91535f9
WuTong <72345506+bestWuTong@users.noreply.github.com>
2026-05-28 17:55:15 +0800

    移除未使用的通知依赖

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -4,7 +4,6 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   desktop_drop
-  local_notifier
   screen_retriever_linux
   tray_manager
   url_launcher_linux
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,6 @@ import Foundation
 
 import desktop_drop
 import file_picker
-import flutter_local_notifications
-import local_notifier
 import screen_retriever_macos
 import shared_preferences_foundation
 import tray_manager
@@ -18,8 +16,6 @@ import window_manager
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
-  LocalNotifierPlugin.register(with: registry.registrar(forPlugin: "LocalNotifierPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   TrayManagerPlugin.register(with: registry.registrar(forPlugin: "TrayManagerPlugin"))
